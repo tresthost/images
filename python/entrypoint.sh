5df840fb50637e0d3c7905e8879b3ac492c13132
JanjyTapYT <janjytapyt@sdevs.org>
2023-08-26 12:07:10 +0200

    Update entrypoint.sh

--- python/entrypoint.sh
+++ python/entrypoint.sh
@@ -40,10 +40,10 @@ python --version
 # Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
 # variable format of "${VARIABLE}" before evaluating the string and automatically
 # replacing the values.
-PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
 
 # Display the command we're running in the output, and then execute it with the env
 # from the container itself.
-printf "\033[1m\033[33mcontainer@tresthost~ \033[0m%s\n" "$PARSED"
+printf "\033[1m\033[33mcontainer@tresthost~ \033[0m%s\n" "${PARSED}"
 # shellcheck disable=SC2086
-exec env ${PARSED}
+eval "${PARSED}"
