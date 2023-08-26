9794dca68dc646d20c33d651c95ceae0aae2e4d1
JanjyTapYT <janjytapyt@sdevs.org>
2023-08-26 20:49:01 +0200

    Update server_startup.sh

--- package/commands/server_startup.sh
+++ package/commands/server_startup.sh
@@ -3,21 +3,21 @@ if [[ $PACKAGE_MANAGER == "npm" ]]; then
     if [[ ! -z $NODE_PACKAGES ]]; then /usr/local/bin/npm install $NODE_PACKAGES; fi;
     if [[ ! -z $UNNODE_PACKAGES ]]; then /usr/local/bin/npm uninstall $UNNODE_PACKAGES; fi;
     if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi;
-    if [[ ! -z $CUSTOM_CMD ]]; then eval "$CUSTOM_CMD"; fi;
+    if [[ ! -z $CUSTOM_CMD ]]; then (cd /home/container/ && eval "$CUSTOM_CMD"); fi;
     /usr/local/bin/node /home/container/$JS_FILE;
 elif [[ $PACKAGE_MANAGER == "pnpm" ]]; then
     if [[ -d .git ]] && [[ $AUTO_UPDATE == "1" ]]; then git pull; fi;
     if [[ ! -z $NODE_PACKAGES ]]; then /usr/local/bin/pnpm install $NODE_PACKAGES; fi;
     if [[ ! -z $UNNODE_PACKAGES ]]; then /usr/local/bin/pnpm remove $UNNODE_PACKAGES; fi;
     if [ -f /home/container/package.json ]; then /usr/local/bin/pnpm install; fi;
-    if [[ ! -z $CUSTOM_CMD ]]; then eval "$CUSTOM_CMD"; fi;
+    if [[ ! -z $CUSTOM_CMD ]]; then (cd /home/container/ && eval "$CUSTOM_CMD"); fi;
     /usr/local/bin/node /home/container/$JS_FILE;
 elif [[ $PACKAGE_MANAGER == "yarn" ]]; then
     if [[ -d .git ]] && [[ $AUTO_UPDATE == "1" ]]; then git pull; fi;
     if [[ ! -z $NODE_PACKAGES ]]; then /usr/local/bin/yarn add $NODE_PACKAGES; fi;
     if [[ ! -z $UNNODE_PACKAGES ]]; then /usr/local/bin/yarn remove $UNNODE_PACKAGES; fi;
     if [ -f /home/container/package.json ]; then /usr/local/bin/yarn; fi;
-    if [[ ! -z $CUSTOM_CMD ]]; then eval "$CUSTOM_CMD"; fi;
+    if [[ ! -z $CUSTOM_CMD ]]; then (cd /home/container/ && eval "$CUSTOM_CMD"); fi;
     /usr/local/bin/node /home/container/$JS_FILE;
 else
     echo "Unsupported PACKAGE_MANAGER: $PACKAGE_MANAGER";
